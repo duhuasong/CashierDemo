1f230880d00e4676e2ce77c2cd536e6b3dee0d2a
zwwise <zw2012014213@163.com>
2019-08-23 15:45:47 +0800

    commit master init version1.0

--- src/untill/XmlUtils.java
+++ src/untill/XmlUtils.java
@@ -23,8 +23,6 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.HttpServletRequest;
 
 import org.dom4j.Document;
 import org.dom4j.Element;
@@ -38,35 +36,6 @@ import org.xml.sax.InputSource;
  */
 public class XmlUtils {
     
-    /** <一句话功能简述>
-     * <功能详细描述>request转字符串
-     * @param request
-     * @return
-     * @see [类、类#方法、类#成员]
-     */
-    public static String parseRequst(HttpServletRequest request){
-        String body = "";
-        try {
-            ServletInputStream inputStream = request.getInputStream(); 
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-            while(true){
-                String info = br.readLine();
-                if(info == null){
-                    break;
-                }
-                if(body == null || "".equals(body)){
-                    body = info;
-                }else{
-                    body += info;
-                }
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }            
-        return body;
-    }
     
     public static String parseXML(SortedMap<String, String> parameters) {
         StringBuffer sb = new StringBuffer();
@@ -83,41 +52,6 @@ public class XmlUtils {
         }
         sb.append("</xml>");
         return sb.toString();
-    }
-
-    /**
-     * 从request中获得参数Map，并返回可读的Map
-     * 
-     * @param request
-     * @return
-     */
-    public static SortedMap getParameterMap(HttpServletRequest request) {
-        // 参数Map
-        Map properties = request.getParameterMap();
-        // 返回值Map
-        SortedMap returnMap = new TreeMap();
-        Iterator entries = properties.entrySet().iterator();
-        Map.Entry entry;
-        String name = "";
-        String value = "";
-        while (entries.hasNext()) {
-            entry = (Map.Entry) entries.next();
-            name = (String) entry.getKey();
-            Object valueObj = entry.getValue();
-            if(null == valueObj){
-                value = "";
-            }else if(valueObj instanceof String[]){
-                String[] values = (String[])valueObj;
-                for(int i=0;i<values.length;i++){
-                    value = values[i] + ",";
-                }
-                value = value.substring(0, value.length()-1);
-            }else{
-                value = valueObj.toString();
-            }
-            returnMap.put(name, value.trim());
-        }
-        return returnMap;
     }
     
     /**
